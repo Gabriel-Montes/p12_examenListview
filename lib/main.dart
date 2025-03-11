0d4e32e53f6c2ed1c39baedc3c4ea68740018c17
GABRIEL_ALEJANDRO_MONTES_HERNANDEZ <a.22308051281276@cbtis128.edu.mx>
2025-03-11 15:58:03 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Myeamen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Myeamen extends StatelessWidget {
+  const Myeamen({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: ListViewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class ListViewScreen extends StatelessWidget {
+  final List<String> items = [
+    "Apple",
+    "Food",
+    "Product",
+    "Item",
+    "List View",
+    "New App",
+    "Design"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        title: Text(
+          "Gabriel Montes 1276",
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.purple,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.purple,
+                child: Text(
+                  items[index][0],
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              title: Text(
+                items[index],
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
+        shape: CircleBorder(),
       ),
     );
   }
